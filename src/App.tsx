3fcbb84f849c7acae7bb5600635951d286ed91ef
Joshua Magnase <magnasejoshua@gmail.com>
2025-10-21 08:16:11 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from '@/components/theme-provider'
+import Home from '@/pages/Home'
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="mindspace-ui-theme">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ThemeProvider>
+  )
+}
+
+export default App
